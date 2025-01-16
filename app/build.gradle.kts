2b7e224beaafec843d035fcdb357aed84452e0b7
suparmanbaduy82 <suparmanbaduy82@gmail.com>
2025-01-17 01:13:09 +0700

    edit activity_login.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.suparman.stockinventorycontrol"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.suparman.stockinventorycontrol"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
