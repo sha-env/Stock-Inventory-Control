34dcaf4e543bfedf72607ceaade3aefd0de2e495
Guan <guanshiyinnn@gmail.com>
2025-01-17 21:46:23 +0700

    Update Button Function

--- app/src/main/java/com/suparman/stockinventorycontrol/MainActivity.kt
+++ app/src/main/java/com/suparman/stockinventorycontrol/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.suparman.stockinventorycontrol
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Tambahkan ini untuk menangani klik tombol
+        val buttonNavigate = findViewById<Button>(R.id.btn_1)
+        buttonNavigate.setOnClickListener {
+            navigateToLoginActivity()
+        }
+
+        // Tambahkan ini untuk menangani klik tombol Register
+        val buttonRegister = findViewById<Button>(R.id.btn_2)
+        buttonRegister.setOnClickListener {
+            navigateToRegisterActivity()
+        }
+    }
+
+    // Fungsi untuk berpindah ke SecondActivity
+    private fun navigateToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    // Fungsi untuk berpindah ke RegisterActivity
+    private fun navigateToRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
