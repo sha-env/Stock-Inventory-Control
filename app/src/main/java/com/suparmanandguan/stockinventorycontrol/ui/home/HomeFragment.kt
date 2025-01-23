62558379d242bdeccb4d865a31c9047ba32b1944
guanshiyin28 <guanshiyinnn@gmail.com>
2025-01-23 09:50:55 +0700

    Main Menu Layout

--- app/src/main/java/com/suparmanandguan/stockinventorycontrol/ui/home/HomeFragment.kt
+++ app/src/main/java/com/suparmanandguan/stockinventorycontrol/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.suparmanandguan.stockinventorycontrol.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,21 +16,11 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
